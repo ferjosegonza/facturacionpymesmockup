4b970dc39fdd042431dbb908e2ea53adc64d4b37
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2024-03-21 09:26:15 -0300

    navegacion entre paginas
    listado proyectos
    listado gastos de proyecto
    agregar gasto
    (falta agregar proveedor)

---
+++ script.js
@@ -0,0 +1,18 @@
+function mostrarDetalle(idProyecto) {
+    document.getElementById('listado_proyectos').style.display = 'none';
+    document.getElementById('listado_gasto').style.display = 'block';
+    document.getElementById('gasto-id').textContent = idProyecto;
+}
+
+function agregarGasto() {
+    document.getElementById('listado_proyectos').style.display = 'none';
+    document.getElementById('listado_gasto').style.display = 'none';
+    document.getElementById('registrar_gasto').style.display = 'block';
+    // document.getElementById('agregar-gasto-id').textContent = ;
+}
+
+function volverListadoProyectos() {
+    document.getElementById('listado_proyectos').style.display = 'block';
+    document.getElementById('listado_gasto').style.display = 'none';
+    document.getElementById('registrar_gasto').style.display = 'none';
+}
